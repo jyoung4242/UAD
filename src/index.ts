a966cd45f0d72e225cd39793bb06ffb47aad4a31
Justin Young <justin_dean_young@yahoo.com>
2022-12-16 19:15:30 -0500

    space

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Container } from "./components/Container";
 
 const template = `
     <div class="container">
-      <\${Container === greet}>
+      <\${ Container === greet}>
     </div>
 `;
 
